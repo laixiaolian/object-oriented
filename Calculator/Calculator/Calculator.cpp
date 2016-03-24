0b8ec9f5f83ea7b152f1ab6be5f0807a5584c50a
laixiaolian <2668136613@qq.com>
2016-03-24 13:18:56 +0800

    code finish

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -3,42 +3,42 @@
 bool Scan::ToStringQueue(string input)
 {
 	size_t markOfReadPos = 0; //标识已经导入到input的第几位,初始0为input第一位
-	bool ifDigit = false; //标识当前读到的字符是否是数字
-	bool ifdecimal = false; //标识是否有小数
+	bool isDigit = false; 
+	bool isdecimal = false; 
 	
 	for (size_t i = 0; i != input.size(); i++)
 	{
 		if (isdigit(input[i]) || input[i] == '.') //判断是否为数字,包括小数点
 		{
-			if (input[i] == '.') //说明此刻的数有小数部分
+			if (input[i] == '.') 
 			{
-				ifdecimal = true;	
+				isdecimal = true;	
 			}
 			
-			ifDigit = true;
+			isDigit = true;
 			continue;
 		}
 
 
-		else //为符号 + - * % （ ）
+		else 
 		{
-			if (ifDigit == true) //判断是否刚读完一串数字
+			if (isDigit == true) //判断是否刚读完一串数字
 			{
-				ifDigit = false;
+				isDigit = false;
 				string tmp = input.substr(markOfReadPos,i - markOfReadPos); //i - markOfReadPos为要截取的字符串字符数
 
 				//判断输入的数字是否超过10位
 
-				if (ifdecimal) //有小数位时，要排除小数点“.”占用位
+				if (isdecimal) 
 				{
-					if ((tmp.size() - 1) > 10) //整数部分 + 小数部分 超过10位
+					if ((tmp.size() - 1) > 10) 
 					{	
 						return false;	
 					}
 		    	}
 
 
-				else //整数
+				else 
 				{
 					if (tmp.size() > 10) //整数部分超过10位
 					{	
@@ -49,7 +49,7 @@ bool Scan::ToStringQueue(string input)
 
 				m_scanstring.push(tmp); //将markOfReadPos 到 i位置的数字 的string导入queue
 				markOfReadPos = i;
-				ifdecimal = false;
+				isdecimal = false;
 			}
 
 
@@ -61,25 +61,25 @@ bool Scan::ToStringQueue(string input)
 	}
 
 
-	if (ifDigit) //表示表达式最后为数字
+	if (isDigit) //表示表达式最后为数字
 	{
 		string tmp = input.substr(markOfReadPos,input.size() - markOfReadPos); //i - markOfReadPos为要截取的字符串字符数
 
 
 		//判断输入的数字是否超过10位
 
-		if (ifdecimal) //有小数位时，要排除小数点“.”占用位
+		if (isdecimal) 
 		{	
-			if ((tmp.size() - 1) > 10) //整数部分 + 小数部分 超过10位
+			if ((tmp.size() - 1) > 10) 
 			{	
 				return false;	
 			}	
 		}
 
 
-		else //整数
+		else 
 		{	
-	    	if (tmp.size() > 10) //整数部分超过10位	
+	    	if (tmp.size() > 10) 
 			{	
 				return false;	
 			}
